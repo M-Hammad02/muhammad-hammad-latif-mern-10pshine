1f8ba38945509f8c2a6f39d518cd31feaa7bb3db
Hammad <muhammadhammadlatif2002@gmail.com>
2025-11-07 07:33:17 +0500

    Setup NoteEditor Module structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Layout/Navbar";
 import ProtectedRoute from "./components/Layout/ProtectedRoute";
-import ForgotPassword from "./components/Auth/ForgotPassword";
-import ResetPassword from "./components/Auth/ResetPassword";
-
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
-import DashboardPage from "./pages/Dashboard";
 import EditorPage from "./pages/EditorPage";
-import ProfilePage from "./pages/ProfilePage";
 
 export default function App(){
   const [query, setQuery] = useState("");
@@ -22,13 +14,7 @@ export default function App(){
           <Navbar query={query} setQuery={setQuery} filterBy={filterBy} setFilterBy={setFilterBy} />
           <main className="flex-1">
             <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
               <Route path="/add" element={<ProtectedRoute><EditorPage /></ProtectedRoute>} />
-              <Route path="/" element={<ProtectedRoute><DashboardPage query={query} setQuery={setQuery} filterBy={filterBy} setFilterBy={setFilterBy} /></ProtectedRoute>} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/reset-password/:token" element={<ResetPassword />} />
             </Routes>
           </main>
         </div>
