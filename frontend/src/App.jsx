1b16e5aa8cfa97f40c66afe58f65ce18074d6c80
Hammad <muhammadhammadlatif2002@gmail.com>
2025-11-07 07:49:58 +0500

    added authprovider function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Layout/Navbar";
 import DashboardPage from "./pages/Dashboard";
 import ProtectedRoute from "./components/Layout/ProtectedRoute";
